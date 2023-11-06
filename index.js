a3a066f6295545f5327e8ae2cbfa77e4d18a2bad
Fabio Flauzino <fabio@mfrural.com.br>
2023-11-06 11:30:15 -0300

    subindo código

---
+++ index.js
@@ -0,0 +1,68 @@
+/**
+Crie uma classe generica que represente um herói de uma aventura e que possua as seguintes propriedades:
+
+- nome
+- idade
+- tipo (ex: guerreiro, mago, monge, ninja )
+
+além disso, deve ter um método chamado atacar que deve atender os seguientes requisitos:
+
+- exibir a mensagem: "o {tipo} atacou usando {ataque}")
+- aonde o {tipo} deve ser concatenando o tipo que está na propriedade da classe
+- e no {ataque} deve seguir uma descrição diferente conforme o tipo, seguindo a tabela abaixo:
+
+se mago -> no ataque exibir (usou magia)
+se guerreiro -> no ataque exibir (usou espada)
+se monge -> no ataque exibir (usou artes marciais)
+se ninja -> no ataque exibir (usou shuriken)
+
+## Saída
+
+Ao final deve se exibir uma mensagem:
+
+- "o {tipo} atacou usando {ataque}"
+  ex: mago atacou usando magia
+  guerreiro atacou usando espada
+ * 
+ */
+
+class Heroi {
+  constructor(nome, idade, tipo) {
+    this.nome = nome
+    this.idade = idade
+    this.tipo = tipo.toLowerCase()
+  }
+
+  atacar(tipo) {
+    let ataque
+    tipo = this.tipo
+    switch (tipo) {
+      case 'mago':
+        ataque = 'usou magia'
+        console.log(`o ${tipo} atacou usando ${ataque}`)
+        break;
+      case 'guerreiro':
+        ataque = 'usou espada'
+        console.log(`o ${tipo} atacou usando ${ataque}`)
+        break;
+      case 'monge':
+        ataque = 'usou artes marciais'
+        console.log(`o ${tipo} atacou usando ${ataque}`)
+        break;
+      case 'ninja':
+        ataque = 'usou shuriken'
+        console.log(`o ${tipo} atacou usando ${ataque}`)
+        break;
+
+      default:
+        ataque = 'usou os próprios punhos'
+        console.log(`o ${tipo} atacou usando ${ataque}`)
+        break;
+    }
+  }
+}
+
+let Mago = new Heroi("EU MESMO", 20, "guerreiro")
+console.log(Mago.atacar())
+
+
